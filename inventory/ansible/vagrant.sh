dbbe9c72cd9ff32443ef36627232cdb64db8b119
Majid d <majidaldosari-github@yahoo.com>
2015-06-24 10:20:23 -0400

    makes a blank vagrantfile if it doesn't exist

--- inventory/ansible/vagrant.sh
+++ inventory/ansible/vagrant.sh
@@ -10,6 +10,13 @@ vagrantfile_dir=${SCRIPTPATH}/../../.vagrant
 vagrantinventory_dir=${SCRIPTPATH}/../vagrant
 vagrantinventory=${vagrantinventory_dir}/vagrant.py
 
+vagrantfile_fn=Vagrantfile
+vagrantfile_pth=${vagrantfile_dir}/${vagrantfile_fn}
+if [ ! -f ${vagrantfile_pth} ]; then
+    #echo "File not found!"
+    touch ${vagrantfile_pth}
+fi
+
 
 cd $vagrantfile_dir
 #passing all params to vagrant inventory script
