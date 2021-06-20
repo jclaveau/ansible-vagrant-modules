2ae5c5bfafd848d6a139644a26f40d9b0ab68861
Jean Claveau <jean.claveau@gmail.com>
2021-06-20 19:26:48 +0200

    Gather tests about add_host issue #22

--- tests.sh
+++ tests.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 # integration tests
-ansible-test integration --python 3.8 --coverage --color
+# --no-temp-workdir avoids Vagrant creation of vms in directories which are deleted after the run
+ansible-test integration --python 3.8 --coverage --color --no-temp-workdir
 ansible-test coverage xml --color
 
 # integration tests which MUST be run from a playbook
-./tests/integration_playbook/tests.sh
+./tests/integration/targets/add_host/in_playbook/tests.sh
 
 # sanity tests
 ansible-test sanity --python 3.8
